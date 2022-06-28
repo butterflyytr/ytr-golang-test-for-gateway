6823b3fbcd45637c047c81094f3d34fa7562b20d
yintingru <yintingru@bytedance.com>
2022-06-28 11:04:28 +0800

    add main.go and run.sh

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/butterflyytr/ytr-golang-test-for-gateway
+
+go 1.16
+
+require github.com/volcengine/vefaas-golang-runtime v0.0.0-20220324111123-998a3428ddcb
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/volcengine/vefaas-golang-runtime/events"
+	"github.com/volcengine/vefaas-golang-runtime/vefaas"
+	"github.com/volcengine/vefaas-golang-runtime/vefaascontext"
+)
+
+func main() {
+	// Start your vefaas function =D.
+	vefaas.Start(handler)
+}
+
+// Define your handler function.
+func handler(ctx context.Context, r *events.HTTPRequest) (*events.EventResponse, error) {
+	fmt.Printf("received new request: %s %s, request id: %s\n", r.HTTPMethod, r.Path, vefaascontext.RequestIdFromContext(ctx))
+	fmt.Printf("debug request: header:%v, body:%s\n", r.Headers, r.Body)
+	ret := make(map[string]interface{})
+	ret["content"] = "Hello From QA YTR Test For gateway!"
+	ret["http_method"] = r.HTTPMethod
+	ret["http_path"] = r.Path
+	query := make(map[string]interface{})
+	header := make(map[string]interface{})
+	for k, v := range r.QueryStringParameters {
+		query[k] = v
+	}
+	for k, v := range r.Headers {
+		header[k] = v
+	}
+	ret["http_query"] = query
+	ret["http_header"] = header
+	ret["http_body"] = string(r.Body)
+	retBody, _ := json.Marshal(ret)
+	return &events.EventResponse{
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: retBody,
+	}, nil
+}
