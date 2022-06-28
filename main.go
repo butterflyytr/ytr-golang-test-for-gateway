482ecce01c629960732bee65fdfcec10572a07d7
yintingru <yintingru@bytedance.com>
2022-06-28 11:43:11 +0800

    add openapi

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
 	"github.com/volcengine/vefaas-golang-runtime/events"
 	"github.com/volcengine/vefaas-golang-runtime/vefaas"
@@ -34,6 +38,8 @@ func handler(ctx context.Context, r *events.HTTPRequest) (*events.EventResponse,
 	ret["http_query"] = query
 	ret["http_header"] = header
 	ret["http_body"] = string(r.Body)
+	openapi_body := AccessOpenApi()
+	ret["openapi_body"] = openapi_body
 	retBody, _ := json.Marshal(ret)
 	return &events.EventResponse{
 		Headers: map[string]string{
@@ -41,4 +47,42 @@ func handler(ctx context.Context, r *events.HTTPRequest) (*events.EventResponse,
 		},
 		Body: retBody,
 	}, nil
+}
+
+func AccessOpenApi() string {
+
+	//url := "https://developer.toutiao.com/api/apps/qrcode"
+	host := "dev.douyincloud.gateway.egress.ivolces.com"
+	url := fmt.Sprintf("http://%s/api/v2/tags/text/antidirt", host)
+	method := "POST"
+
+	//payload := strings.NewReader(`{"access_token": "0801121846765a5a4d2f6b385a68307237534d43397a667865513d3d","appname": "douyin"}`)
+	payloadWithoutToken := strings.NewReader(`{"tasks": [{"content": "要检测的文本"}]}`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payloadWithoutToken)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	//req.Header.Add("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	resheader := res.Header
+	log.Printf("ytr test raw request:%+v\n\n", req)
+	log.Printf("ytr test resp from openapi:%+v,%+v\n\n", string(body), resheader)
+	return string(body)
 }
