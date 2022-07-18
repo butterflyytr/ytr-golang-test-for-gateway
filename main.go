5d0168093aa4c7a3e42012e4ff1633569cc6b004
yintingru <yintingru@bytedance.com>
2022-07-18 20:46:20 +0800

    add AccessOpenApi3()

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/volcengine/vefaas-golang-runtime/events"
+	"github.com/volcengine/vefaas-golang-runtime/vefaas"
+	"github.com/volcengine/vefaas-golang-runtime/vefaascontext"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
-
-	"github.com/volcengine/vefaas-golang-runtime/events"
-	"github.com/volcengine/vefaas-golang-runtime/vefaas"
-	"github.com/volcengine/vefaas-golang-runtime/vefaascontext"
 )
 
 func main() {
@@ -38,10 +37,13 @@ func handler(ctx context.Context, r *events.HTTPRequest) (*events.EventResponse,
 	ret["http_query"] = query
 	ret["http_header"] = header
 	ret["http_body"] = string(r.Body)
-	//openapi_url, openapi_body := AccessOpenApi()
-	openapi_url, openapi_body := AccessOpenApi2()
+	openapi_url, openapi_body := AccessOpenApi()
+	//openapi_url, openapi_body := AccessOpenApi2()
 	ret["openapi_body"] = openapi_body
 	ret["openapi_url"] = openapi_url
+	openapi_url3, openapi_body3 := AccessOpenApi3()
+	ret["openapi_body3"] = openapi_body3
+	ret["openapi_url3"] = openapi_url3
 	retBody, _ := json.Marshal(ret)
 	return &events.EventResponse{
 		Headers: map[string]string{
@@ -107,6 +109,48 @@ func AccessOpenApi2() (string, string) {
 
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payloadWithoutToken)
+
+	if err != nil {
+		fmt.Println(err)
+		return "", ""
+	}
+	//req.Header.Add("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return "", ""
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return "", ""
+	}
+	resheader := res.Header
+	log.Printf("ytr test raw request:%+v\n\n", req)
+	log.Printf("ytr test resp from openapi:%+v,%+v\n\n", string(body), resheader)
+	return url, string(body)
+}
+
+func AccessOpenApi3() (string, string) {
+
+	//url := "https://developer.toutiao.com/api/apps/qrcode"
+	//host := "dev.douyincloud.gateway.egress.ivolces.com"
+	//host := "douyincloud.gateway.egress.ivolces.com"
+	host := "developer.toutiao.com"
+	//url=http://developer-boe.toutiao.com/api/apps/customer_service/url?appid=ttded7a86a41b127b6&access_token=080112184676786c51723461725a346f4e536d342f5242544b673d3d&openid=76f096e2-47c8-4473-8621-71ee839f6378&type=1128&scene=1
+
+	url := fmt.Sprintf("http://%s/api/apps/customer_service/url?appid=tt1782613f88e45ec801&openid=76f096e2-47c8-4473-8621-71ee839f6378&type=1128&scene=1", host)
+	method := "GET"
+
+	//payload := strings.NewReader(`{"access_token": "0801121846765a5a4d2f6b385a68307237534d43397a667865513d3d","appname": "douyin"}`)
+	//payloadWithoutToken := strings.NewReader(`{"appname": "douyin"}`)
+	//payloadWithoutToken := strings.NewReader(`{"appname": "douyin"}`)
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
 		fmt.Println(err)
